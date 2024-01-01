b24048c2b6990cce03f2a61327d890d83c580c65
cijb-7724 <johokamia0@gmail.com>
2024-01-01 15:25:20 +0900

    titanic

--- classification-titanic/titanic_more_features.cpp
+++ classification-titanic/titanic_more_features.cpp
@@ -106,7 +106,7 @@ void test_output(vector<layer_t> &nn, int depth) {
     normalization_test(test_x, passengers);
     int test_size = test_x.size();
 
-    // valid set-------------------------------------
+    // test set-------------------------------------
     for (int i=0; i<40; ++i) cout << "=";
     cout << endl;
     cout << "test set" << endl;
@@ -175,13 +175,13 @@ int main() {
         }
     }
     
-    double eta = 0.03, attenuation = 0.6;
+    double eta = 0.03, attenuation = 0.8;
     int show_interval = 100;
-    int learning_plan = 1000;
-    int loop = 10000;
-    int batch_size = 100; //<train_size
-    vector<int> nn_form = {4, 32, 32, 32, 2};
-    // vector<int> nn_form = {4, 100, 200, 2};
+    int learning_plan = 100;
+    int loop = 2000;
+    int batch_size = 200; //<train_size
+    vector<int> nn_form = {7, 32, 32, 32, 2};
+    vector<int> nn_form = {7, 14, 28, 56, 40, 20, 40, 20, 10, 5, 2};
     
     int depth = nn_form.size()-1;
 
@@ -401,7 +401,7 @@ vector<string> split(const string &s, char delimiter) {
     return tokens;
 }
 //medianで補完
-//age, fareのみ
+//age, fare, Embarkedのみ
 void missing_value(vector<passenger_t> &psg) {
     int n = psg.size();
     vector<int> age;
@@ -420,6 +420,10 @@ void missing_value(vector<passenger_t> &psg) {
     for (int i=0; i<n; ++i) {
         if (psg[i].Fare == -1) psg[i].Fare = fare[fare.size()/2];
     }
+    // embarked
+    for (int i=0; i<n; ++i) {
+        if (psg[i].Embarked == ' ') psg[i].Embarked = 'S';
+    }
 }
 
 void normalization(vvd &x, vvd &t, vector<passenger_t> &psg) {
@@ -428,38 +432,61 @@ void normalization(vvd &x, vvd &t, vector<passenger_t> &psg) {
     int n = psg.size();
     //正規化と特徴量エンジニアリング
     /*
-    Pclass:金持ち権力出ちゃうかも　いる！
-    Name:名前は確認しないだろう　いらない！
-    Sex:これはありそう　いる！
-    Age:丈夫な体の人は寒くても生き残るだろう　いる！
-    SibSp:とっさにそこを考慮して優先してボートに乗せるか？　いらない！
-    Parch:とっさにそこを考慮して優先してボートに乗せるか？　いらない！
-    Ticket:チケットも確認されないだろう　いらない！
-    Fare:金持ち権力出ちゃうかも　いる！
-    Cabin:部屋も金持ちと相関あるだろうけど，欠損値が多すぎるから捨てる　いらない！
-    Embarked:港も今回は無視　いらない！
-    Pclass, Sex, Age, Fare
+    Pclass:iru
+    Name:iranai
+    Sex:iru
+    Age:iru
+    SibSp:iru
+    Parch:iru
+    Ticket:iranai muzui
+    Fare:iru
+    Cabin:iranai
+    Embarked:iru
+    Pclass, Sex, Age, SibSp, Parch, Fare, Embarked
     */
-    vd vec_Pclass, vec_Sex, vec_Age, vec_Fare;
+    vd vec_Pclass, vec_Sex, vec_Age, vec_SibSp, vec_Parch, vec_Fare, vec_Embarked;
     for (int i=0; i<n; ++i) {
         vec_Pclass.push_back(psg[i].Pclass);
         if (psg[i].Sex == "male") vec_Sex.push_back(0);
         else vec_Sex.push_back(1);
         vec_Age.push_back(psg[i].Age);
+        vec_SibSp.push_back(psg[i].SibSp);
+        vec_Parch.push_back(psg[i].Parch);
         vec_Fare.push_back(psg[i].Fare);
+        if (psg[i].Embarked == 'S') vec_Embarked.push_back(1);
+        else if (psg[i].Embarked == 'C') vec_Embarked.push_back(2.0/3);
+        else vec_Embarked.push_back(1.0/3);
     }
-    double mx_Pclass, mn_Pclass, mx_Age, mn_Age, mx_Fare, mn_Fare;
+    double mx_Pclass, mn_Pclass, mx_Age, mn_Age, mx_SibSp, mn_SibSp;
+    double mx_Parch, mn_Parch, mx_Fare, mn_Fare, mx_Embarked, mn_Embarked;
     mx_Pclass = *max_element(vec_Pclass.begin(), vec_Pclass.end());
     mn_Pclass = *min_element(vec_Pclass.begin(), vec_Pclass.end());
     mx_Age = *max_element(vec_Age.begin(), vec_Age.end());
     mn_Age = *min_element(vec_Age.begin(), vec_Age.end());
+    mx_SibSp = *max_element(vec_SibSp.begin(), vec_SibSp.end());
+    mn_SibSp = *min_element(vec_SibSp.begin(), vec_SibSp.end());
+    mx_Parch = *max_element(vec_Parch.begin(), vec_Parch.end());
+    mn_Parch = *min_element(vec_Parch.begin(), vec_Parch.end());
     mx_Fare = *max_element(vec_Fare.begin(), vec_Fare.end());
     mn_Fare = *min_element(vec_Fare.begin(), vec_Fare.end());
+    mx_Embarked = *max_element(vec_Embarked.begin(), vec_Embarked.end());
+    mn_Embarked = *min_element(vec_Embarked.begin(), vec_Embarked.end());
 
     for (int i=0; i<n; ++i) {
         vec_Pclass[i] = (vec_Pclass[i] - mn_Pclass) / (mx_Pclass - mn_Pclass);
         vec_Age[i] = (vec_Age[i] - mn_Age) / (mx_Age - mn_Age);
+        vec_SibSp[i] = (vec_SibSp[i] - mn_SibSp) / (mx_SibSp - mn_SibSp);
+        vec_Parch[i] = (vec_Parch[i] - mn_Parch) / (mx_Parch - mn_Parch);
         vec_Fare[i] = (vec_Fare[i] - mn_Fare) / (mx_Fare - mn_Fare);
+        vec_Embarked[i] = (vec_Embarked[i] - mn_Embarked) / (mx_Embarked - mn_Embarked);
+    }
+    for (int i=0; i<n; ++i) {
+        vec_Pclass[i] = 2 * vec_Parch[i] - 1;
+        vec_Age[i] = 2 * vec_Age[i] - 1;
+        vec_SibSp[i] = 2 * vec_SibSp[i] - 1;
+        vec_Parch[i] = 2 * vec_Parch[i] - 1;
+        vec_Fare[i] = 2 * vec_Fare[i] - 1;
+        vec_Embarked[i] = 2 * vec_Embarked[i] - 1;
     }
 
     for (int i=0; i<n; ++i) {
@@ -467,38 +494,61 @@ void normalization(vvd &x, vvd &t, vector<passenger_t> &psg) {
         if (psg[i].Survived == 1) t.push_back({1, 0});
         else t.push_back({0, 1});
         //訓練インスタンス
-        x.push_back({vec_Pclass[i], vec_Sex[i], vec_Age[i], vec_Fare[i]});
+        x.push_back({vec_Pclass[i], vec_Sex[i], vec_Age[i], vec_SibSp[i], vec_Parch[i], vec_Fare[i], vec_Embarked[i]});
     }
 }
 
 void normalization_test(vvd &x, vector<passenger_t> &psg) {
     x.assign(0, vd(0));
     int n = psg.size();
-    vd vec_Pclass, vec_Sex, vec_Age, vec_Fare;
+    vd vec_Pclass, vec_Sex, vec_Age, vec_SibSp, vec_Parch, vec_Fare, vec_Embarked;
     for (int i=0; i<n; ++i) {
         vec_Pclass.push_back(psg[i].Pclass);
         if (psg[i].Sex == "male") vec_Sex.push_back(0);
         else vec_Sex.push_back(1);
         vec_Age.push_back(psg[i].Age);
+        vec_SibSp.push_back(psg[i].SibSp);
+        vec_Parch.push_back(psg[i].Parch);
         vec_Fare.push_back(psg[i].Fare);
+        if (psg[i].Embarked == 'S') vec_Embarked.push_back(1);
+        else if (psg[i].Embarked == 'C') vec_Embarked.push_back(2.0/3);
+        else vec_Embarked.push_back(1.0/3);
     }
-    double mx_Pclass, mn_Pclass, mx_Age, mn_Age, mx_Fare, mn_Fare;
+    double mx_Pclass, mn_Pclass, mx_Age, mn_Age, mx_SibSp, mn_SibSp;
+    double mx_Parch, mn_Parch, mx_Fare, mn_Fare, mx_Embarked, mn_Embarked;
     mx_Pclass = *max_element(vec_Pclass.begin(), vec_Pclass.end());
     mn_Pclass = *min_element(vec_Pclass.begin(), vec_Pclass.end());
     mx_Age = *max_element(vec_Age.begin(), vec_Age.end());
     mn_Age = *min_element(vec_Age.begin(), vec_Age.end());
+    mx_SibSp = *max_element(vec_SibSp.begin(), vec_SibSp.end());
+    mn_SibSp = *min_element(vec_SibSp.begin(), vec_SibSp.end());
+    mx_Parch = *max_element(vec_Parch.begin(), vec_Parch.end());
+    mn_Parch = *min_element(vec_Parch.begin(), vec_Parch.end());
     mx_Fare = *max_element(vec_Fare.begin(), vec_Fare.end());
     mn_Fare = *min_element(vec_Fare.begin(), vec_Fare.end());
+    mx_Embarked = *max_element(vec_Embarked.begin(), vec_Embarked.end());
+    mn_Embarked = *min_element(vec_Embarked.begin(), vec_Embarked.end());
 
     for (int i=0; i<n; ++i) {
         vec_Pclass[i] = (vec_Pclass[i] - mn_Pclass) / (mx_Pclass - mn_Pclass);
         vec_Age[i] = (vec_Age[i] - mn_Age) / (mx_Age - mn_Age);
+        vec_SibSp[i] = (vec_SibSp[i] - mn_SibSp) / (mx_SibSp - mn_SibSp);
+        vec_Parch[i] = (vec_Parch[i] - mn_Parch) / (mx_Parch - mn_Parch);
         vec_Fare[i] = (vec_Fare[i] - mn_Fare) / (mx_Fare - mn_Fare);
+        vec_Embarked[i] = (vec_Embarked[i] - mn_Embarked) / (mx_Embarked - mn_Embarked);
+    }
+    for (int i=0; i<n; ++i) {
+        vec_Pclass[i] = 2 * vec_Parch[i] - 1;
+        vec_Age[i] = 2 * vec_Age[i] - 1;
+        vec_SibSp[i] = 2 * vec_SibSp[i] - 1;
+        vec_Parch[i] = 2 * vec_Parch[i] - 1;
+        vec_Fare[i] = 2 * vec_Fare[i] - 1;
+        vec_Embarked[i] = 2 * vec_Embarked[i] - 1;
     }
 
     for (int i=0; i<n; ++i) {
         //訓練インスタンス
-        x.push_back({vec_Pclass[i], vec_Sex[i], vec_Age[i], vec_Fare[i]});
+        x.push_back({vec_Pclass[i], vec_Sex[i], vec_Age[i], vec_SibSp[i], vec_Parch[i], vec_Fare[i], vec_Embarked[i]});
     }
 }
 
